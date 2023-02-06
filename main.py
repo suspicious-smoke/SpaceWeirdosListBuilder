c7841e2c5beeb582319092003cc35e4334aba287
Jacob <jacobneal65@gmail.com>
2023-02-05 22:15:13 -0600

    test

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
-# This is a sample Python script.
+import logic as h
+from flask import Flask, render_template, request
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+app = Flask(__name__)
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.route("/", methods=['GET'])
+def index():
+    return render_template('test.html')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@app.route('/result/', methods=['POST'])
+def result():
+    # get number of users from form
+    number_of_users = request.form['number_of_users']
+    is_success = False
+    # if number of users is not empty continue
+    if number_of_users:
+        user_list = h.GetUsers(number_of_users)
+        if isinstance(user_list, str):
+            status = user_list
+        else:
+            # create csv file and check if it is successful
+            status = h.CreateCsv(user_list)
+            if status == "finished creating user list csv":
+                is_success = True
+    else:
+        status = "no input"
+    # render webpage
+    return render_template('test.html', number_of_users=number_of_users, status=status, success=is_success)
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=8080, debug=True)

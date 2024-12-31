aa549bb751bf30cd4ba433a729383dbf10cb9c63
Jacob <jacobneal65@gmail.com>
2024-12-31 14:53:14 -0600

    work on warband page

--- static/warband.js
+++ static/warband.js
@@ -1,40 +1,17 @@
 window.onload = function() {
-    // check warband id
-    const warband_id = document.getElementById('warband_id').value
-
-    const json_warband = localStorage.getItem('warbands');
-    let warband = null;
-
-    if (json_warband != null) {
-        const warbands = JSON.parse(json_warband)['warbands'];
-        for (const wbnd of warbands) {
-            
-            if (wbnd['warband_id']==warband_id) { // found a saved warband
-                warband = wbnd;
-            }
-
-        }
-    }
-    //if id doesn't exist, reload to id 0
-    if (warband==null && warband_id != 0) {
-        window.location.href = new_warband_url;
-    } else if (warband_id == 0) {
-       document.getElementById('warband_text').innerHTML = "Create Warband";
-    } else {
-        document.getElementById('warband_text').innerHTML = "Edit Warband";
-    }
-
-    // fill in table with warband information
-
+    loadWarband();
 
 
 
     // create new weirdo button
     document.getElementById('create_weirdo').addEventListener('click', function() {
         const warband_id = this.dataset.warband_id;
-        const weirdo_id = 0;
         // clear form for new weirdo
-        
+        document.getElementById('warband_id').value = warband_id;
+        document.getElementById('weirdo_id').value = 0;
+        document.getElementById('weirdo_name').value = '';
+        resetSelect('speed_select');
+        resetSelect('defense_select');
         wireSaveWeirdo();
     });
     
@@ -52,6 +29,35 @@ function wireSaveWeirdo() {
         saveWeirdo();
     });
 }
+
+function loadWarband() {
+    // check warband id
+    const warband_id = document.getElementById('warband_id').value
+
+    const json_warband = localStorage.getItem('warbands');
+    let warband = null;
+
+    if (json_warband != null) {
+        const warbands = JSON.parse(json_warband)['warbands'];
+        for (const wbnd of warbands) {
+            if (wbnd['warband_id']==warband_id) { // found a saved warband
+                warband = wbnd;
+            }
+        }
+    }
+    
+    if (warband==null && warband_id != 0) { //if id doesn't exist, reload to id 0
+        window.location.href = new_warband_url;
+    } else if (warband_id == 0) {
+        document.getElementById('warband_text').innerHTML = "Create Warband";
+    } else {
+        document.getElementById('warband_text').innerHTML = "Edit Warband";
+    }
+
+    // fill in table with warband information
+
+}
+
 
 function saveWeirdo() {
     // get weirdo information from modal
@@ -77,8 +83,13 @@ function saveWeirdo() {
     json_weirdo = JSON.stringify(weirdo); // convert to json
     // save to local storage
     localStorage.setItem('warband_'+warband_id,json_weirdo)
-    
-    
     location.reload(); // reload page
-    
+}
+
+function resetSelect(list_name) {
+    let select_list = document.getElementById(list_name);
+    for (let i=0; i < select_list.options.length; i++) {
+        select_list.options[i].selected = false;
+    }
+    select_list.options[0].selected = true;
 }

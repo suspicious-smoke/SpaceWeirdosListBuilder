8c3ef390ea183e9c9d4008d25df8b329329c3e6b
starbuck <jacobneal65@gmail.com>
2025-01-23 12:49:30 -0600

    work on print page

--- static/print_warband.js
+++ static/print_warband.js
@@ -1,5 +1,10 @@
 import {getLocalData, getWarband, getWarbandPoints} from './local_storage.js';
 
 window.onload = function() {
-    const warband_id = this.dataset.warband_id;
+    const warband_id = document.getElementById('warband_id').value;
+    let warband = getWarband(warband_id);
+    document.getElementById('warband_name').innerHTML =  warband.name;
+    document.getElementById('warband_trait').innerHTML =  warband.warband_trait;
+    document.getElementById('leader_trait').innerHTML =  warband.leader_trait;
+    let x = 4;
 }

bacd1d4580f8c80bb6f9d55fd939eac52c632251
starbuck <jacobneal65@gmail.com>
2025-01-27 21:38:23 -0600

    item point calculations change

--- controller.py
+++ controller.py
@@ -2,18 +2,10 @@ import datetime
 from flask import jsonify, render_template, request, make_response, redirect, url_for
 from models import *
 
-max_age = datetime.timedelta(days=365 * 10)
+
 # Connects to the warbands page
 def home_page():
     return make_response(render_template('home.html'))
-
-# def get_weirdo(warband_id,weirdo_id=0):
-#     weirdo = Weirdo(warband_id,weirdo_id,name="successful test").serialize()
-#     # model = next((m for m in models if m["id"] == weirdo_id), None)
-#     # if model:
-#     #     return jsonify(model)
-#     return jsonify(weirdo)
-#     # return jsonify({"error": "Model not found"}), 404
 
 
 # load warband page
@@ -27,14 +19,6 @@ def print_warband_page(warband_id):
     # get warband id and selects
     _warband = Warband(warband_id)
     return make_response(render_template('print_warband.html', warband = _warband))
-
-
-# gets point value for given item equiped to weirdo
-def item_points(item_list, weirdo_item):
-    for item in item_list:
-            if item['name'] == weirdo_item:
-                return item['points']
-    return 0
 
 
 def get_trait_text():
@@ -80,21 +64,34 @@ def get_weirdo_equipments():
 
 # load warband page
 def warband_points():
+    mutant_weapons = ['Claws & Teeth','Horrible Claws & Teeth','Whip/Tail']
+    soldiers_equipment = ['Grenade*','Heavy Armor','Medkit*']
     # get warband id and selects
     warband = request.get_json()
     points = 0
     weirdo_points_list = []
     for weirdo in warband['weirdos']:
-        weirdo_pts = speed[weirdo['speed']] + defense[weirdo['defense']] + firepower[weirdo['firepower']] + prowess[weirdo['prowess']] + willpower[weirdo['willpower']]
+        speed_discount = 1 if warband['warband_trait'] == 'Mutants' else 0
+        weirdo_pts = max(0,speed[weirdo['speed']] - speed_discount) + defense[weirdo['defense']] + firepower[weirdo['firepower']] + prowess[weirdo['prowess']] + willpower[weirdo['willpower']]
         
+        ranged_discount = 0
+        melee_discount = 0
+        if warband['warband_trait'] == 'Heavily Armed':
+            ranged_discount = 1
+        elif warband['warband_trait'] == 'Mutants' and weirdo['melee_weapon'] in mutant_weapons:
+            melee_discount = 1
+
         if 'ranged_weapon' in weirdo:
-            weirdo_pts += item_points(ranged_weapons, weirdo['ranged_weapon'])
+            weirdo_pts += max(0, item_points(ranged_weapons, weirdo['ranged_weapon']) - ranged_discount)
         if 'melee_weapon' in weirdo:
-            weirdo_pts += item_points(melee_weapons, weirdo['melee_weapon'])
+            weirdo_pts += max(0, item_points(melee_weapons, weirdo['melee_weapon']) - melee_discount)
         
+        
+
         if 'equipment' in weirdo:
             for equip in weirdo['equipment']:
-                weirdo_pts += item_points(equipment, equip)
+                if not (warband['warband_trait'] == 'Soldiers' and equip in soldiers_equipment):
+                    weirdo_pts += item_points(equipment, equip)
         
         if 'powers' in weirdo:
             for power in weirdo['powers']:
@@ -106,3 +103,10 @@ def warband_points():
     # Return the result as JSON
     return jsonify({"points": points, "weirdos": weirdo_points_list})
 
+
+# gets point value for given item equiped to weirdo
+def item_points(item_list, weirdo_item):
+    for item in item_list:
+            if item['name'] == weirdo_item:
+                return item['points']
+    return 0

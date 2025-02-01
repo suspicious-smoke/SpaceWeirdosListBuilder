f407cb26b6b1dae5a6a4b12d890e59978792f023
starbuck <jacobneal65@gmail.com>
2025-01-31 22:44:35 -0600

    move create app outside of name == main logic

--- app.py
+++ app.py
@@ -18,8 +18,7 @@ def create_app():
     # _app.add_url_rule("/create-warband", view_func=controller.warband_create_page, methods=["GET", "POST"])
     return _app
 
-
+app = create_app()
 if __name__ == "__main__":
-    app = create_app()
     port = app.config.get("PORT", 5000)
     app.run(host="0.0.0.0", port=port)

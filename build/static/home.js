296456bf4b51b48d0288674b3862b34a1d30c872
Jacob <jacobneal65@gmail.com>
2025-02-16 15:12:09 -0600

    added build folder

---
+++ build/static/home.js
@@ -0,0 +1,137 @@
+import {getLocalData, getWarbandPoints} from './local_storage.js';
+import { deleteEventListeners } from './helpers.js';
+window.onload = function() {
+    loadWarbandTable();
+    document.getElementById('save_json_warband').addEventListener('click', sendSavedWarbandsToClipboard);
+    document.getElementById('load_warband_text').addEventListener('click', function() {
+        let saved_list = document.getElementById('warband_json_text').value;
+        loadWarbandJson(saved_list);
+    });
+}
+
+
+function loadWarbandTable() {
+    let warbands = getLocalData()['warbands'];
+    let w_table = document.getElementById('warband_table');
+    deleteEventListeners('.delete_warband');
+    deleteEventListeners('.duplicate_warband');
+    if (warbands.length == 0) {
+        document.getElementById('table_area').setAttribute('hidden',true);
+    }
+    for(const wbnd of warbands) {
+        // build table rows
+        let warband_id = wbnd['warband_id']
+        let row = w_table.insertRow();
+        // let w_id = row.insertCell(0);
+        // w_id.innerHTML = warband_id;
+
+        let w_name = row.insertCell(0);
+        w_name.innerHTML = wbnd['name'];
+        
+        let w_trait = row.insertCell(1);
+        w_trait.innerHTML = wbnd['warband_trait'];
+        
+        let w_count = row.insertCell(2);
+        w_count.innerHTML = wbnd['weirdos'].reduce((running_sum, weirdo) => running_sum + parseInt(weirdo.copies),0);
+
+        let w_points = row.insertCell(3);
+        getWarbandPoints(warband_id).then((pts) => {
+            w_points.innerHTML =  pts.points;
+          });
+        
+
+        let buttons = row.insertCell(4);
+        let editbtn = document.createElement('a');
+        editbtn.setAttribute('href',`/warband?warband_id=${warband_id}`);
+        editbtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary');
+        editbtn.innerHTML = `<i style="color:darkgreen;" class="bi bi-pencil-square"></i>`;
+        editbtn.title = 'Edit Warband';
+        buttons.appendChild(editbtn);
+
+        let duplicatebtn = document.createElement('button');
+        duplicatebtn.setAttribute('data-warband_id',warband_id);
+        duplicatebtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary', 'ms-1', 'duplicate_warband');
+        duplicatebtn.innerHTML = `<i style="color:darkgoldenrod;" class="bi bi-copy"></i>`;
+        duplicatebtn.title = 'Duplicate Warband';
+        buttons.appendChild(duplicatebtn);
+
+        let deletebtn = document.createElement('button');
+        deletebtn.setAttribute('data-warband_id',warband_id);
+        deletebtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary', 'ms-1', 'delete_warband');
+        deletebtn.innerHTML = `<i style="color:darkred;" class="bi bi-trash"></i>`;
+        deletebtn.title = 'Delete Warband';
+        buttons.appendChild(deletebtn);
+
+
+        let printbtn = document.createElement('a');
+        printbtn.setAttribute('href',`/print_warband?warband_id=${warband_id}`);
+        printbtn.classList.add('btn', 'btn-sm', 'btn-outline-secondary', 'ms-1', 'print_warband');
+        printbtn.innerHTML = `<i class="text-primary bi bi-printer"></i>`;
+        printbtn.target = '_blank'
+        printbtn.title = 'Print Warband';
+        buttons.appendChild(printbtn);
+
+    }
+    // wire event listener for delete
+    const delete_btns = document.querySelectorAll('.delete_warband');
+    delete_btns.forEach(btn => {
+        btn.addEventListener('click', (btn_elem) => {
+            const confirmed = confirm("Are you sure you want to delete this warband?");
+            if (!confirmed) {
+                return; // Prevent the default action (e.g., form submission)
+              }
+            let warband_id = btn_elem.target.dataset.warband_id;
+            let local_data = getLocalData();
+            const i = local_data['warbands'].findIndex(x => x['warband_id'] == warband_id); // get warband
+            if (i>-1) {
+                // remove entry from warband
+                local_data['warbands'].splice(i,1);
+                localStorage.setItem('warbands', JSON.stringify(local_data));
+                location.reload(); // reload page
+            }
+        });
+    });
+
+
+    const duplicate_btns = document.querySelectorAll('.duplicate_warband');
+    duplicate_btns.forEach(weirdo => {
+        weirdo.addEventListener('click', (btn_elem) => {
+            let warband_id = btn_elem.target.dataset.warband_id;
+            let local_data = getLocalData();
+            const i = local_data['warbands'].findIndex(x => x['warband_id'] == warband_id); // get warband
+            let warband = {...local_data['warbands'][i]}; // copy warband   
+            // get the max id and increase it by 1
+            const newId = Math.max(...local_data['warbands'].map(x => x.warband_id))+1;
+            warband['warband_id'] = newId;
+            local_data['warbands'].push(warband);
+            localStorage.setItem('warbands', JSON.stringify(local_data));
+            location.reload(); // reload page
+        });
+    });
+
+
+    document.getElementById('warband_json_text').value = JSON.stringify(getLocalData());   
+}
+
+
+function sendSavedWarbandsToClipboard() {
+    const json_warband = getLocalData();
+    // Copy the text inside the text field
+    navigator.clipboard.writeText(JSON.stringify(json_warband));
+    // Alert the copied text
+    alert("Copied the text to clipboard:\n\n" + JSON.stringify(json_warband));
+}
+
+
+function loadWarbandJson(saved_list) {
+    // get value from textbox
+    let warbands_json = JSON.parse(document.getElementById('warband_json_text').value);
+
+    // check that the values seem valid
+    if (warbands_json['warbands']) {
+        localStorage.setItem('warbands', JSON.stringify(warbands_json));
+        location.reload();  // reload page (should configure reload warband list instead)
+    } else {
+        alert("json is missing warbands. Double check it is formated correctly.");
+    }
+}
---
+++ build/static/print_warband.js
@@ -0,0 +1,153 @@
+import { warband_traits, leader_traits, speed, defense, firepower, prowess, willpower, melee_weapons, ranged_weapons, equipment, powers} from './formdata.js';
+import { getWarband, getWarbandPoints } from './local_storage.js';
+window.onload = function() {
+    
+
+    const urlParams = new URLSearchParams(window.location.search);
+    const warband_id = urlParams.get('warband_id');
+    document.getElementById('warband_id').value = warband_id;
+    let warband = getWarband(warband_id);
+    document.title = `${warband.name}`;
+    document.getElementById('warband_name').innerHTML =  warband.name;
+    document.getElementById('warband_trait').innerHTML =  '['+warband.warband_trait+']';
+    document.getElementById('leader_trait').innerHTML =  '['+warband.leader_trait+']';
+
+    document.getElementById('warband_trait_text').innerHTML = warband_traits[warband.warband_trait];
+    document.getElementById('leader_trait_text').innerHTML = leader_traits[warband.leader_trait];
+
+
+    loadWeirdoPrintCards(warband); 
+    document.getElementById('one_col_print').checked = false;
+    document.getElementById('one_col_print').addEventListener('click', (checkbox) => {
+        const template_card = document.getElementById('card_template');
+        if (document.getElementById('one_col_print').checked) {
+            template_card.classList.remove('col-6');
+            template_card.classList.add('col-12');
+        } else {
+            template_card.classList.add('col-6');
+            template_card.classList.remove('col-12');
+        }
+        loadWeirdoPrintCards(warband); 
+    });
+    document.getElementById('eq_desc').addEventListener('click', (checkbox) => {
+        loadWeirdoPrintCards(warband); 
+    });
+
+    document.getElementById('hide_validation_summary').addEventListener('click', (checkbox) => {
+        if (document.getElementById('hide_validation_summary').checked) {
+            document.getElementById('validation_row').classList.add('hide_validation');
+        } else {
+            document.getElementById('validation_row').classList.remove('hide_validation');
+        }
+        loadWeirdoPrintCards(warband)
+
+    });
+} 
+
+function loadWeirdoPrintCards(warband) {
+    let weirdos = warband['weirdos'];
+    let card_container = document.getElementById('weirdo_cards');
+    card_container.innerHTML = '';
+    getWarbandPoints(warband['warband_id']).then((data) => {
+        document.getElementById('total_cost').innerHTML = data.points;
+        if (data.validation != '' && !document.getElementById('validation_row').classList.contains('hide_validation')) {
+            document.getElementById('validation_row').removeAttribute('hidden');
+            document.getElementById('validation_text').innerHTML = data.validation;
+        } else {
+            document.getElementById('validation_row').setAttribute('hidden',true);
+        }
+        // create new card for each weirdo
+        let first = true;
+        for (const weirdo of weirdos) {
+            let weirdo_cost = 0
+            // get weirdo points
+            for (const w_pts of data.weirdos) {
+                if (weirdo['weirdo_id'] == w_pts.id) {
+                    weirdo_cost = w_pts.points
+                }
+            }
+            let template_card = document.getElementById('card_template');
+            let new_card = template_card.cloneNode(true); // clear out events
+            
+            new_card.removeAttribute("hidden");
+            new_card.removeAttribute("id");
+            new_card.querySelector('.card-title').innerHTML = weirdo['name'];
+            if (first) {
+                new_card.querySelector('.card-title').innerHTML += '&emsp;[leader]';
+                first = false;
+            }
+            
+            new_card.querySelector('.card-cost').innerHTML = `Cost: ${weirdo_cost} (x${weirdo['copies']})`;
+            if (weirdo['copies']==1) {
+                new_card.querySelector('.card-cost').innerHTML = `Cost: ${weirdo_cost}`;
+            }
+            // card attributes
+            for (const att of weirdo_attribute) {
+                new_card.querySelector(`.card-${att}`).innerHTML = weirdo[att];
+            } 
+            // card weapons
+            
+            let melee = melee_weapons.find(weapon => weapon.name === weirdo['melee_weapon']);
+            new_card.querySelector('.m_name').innerHTML = melee.name;
+            new_card.querySelector('.m_action').innerHTML = 'act: '+ melee.actions;
+            new_card.querySelector('.m_notes').innerHTML = melee.notes;
+
+            
+            if (weirdo['firepower'] != 0) {
+                let ranged = ranged_weapons.find(weapon => weapon.name === weirdo['ranged_weapon']);
+                new_card.querySelector('.r_name').innerHTML = ranged.name;
+                new_card.querySelector('.r_action').innerHTML = 'act: '+ ranged.actions;
+                new_card.querySelector('.r_notes').innerHTML = ranged.notes;
+            } else {
+                new_card.querySelector('.ranged_weapon').setAttribute("hidden", true);
+            }
+            // items
+            let equipment_area = new_card.querySelector('.equipment_area');
+            
+            // Get list of equipment
+            let equipment_list =  [];
+            weirdo.equipment.forEach(eq => {
+                const matchingEquipment = equipment.find(list_eq => list_eq.name === eq);
+                if (matchingEquipment) {
+                    equipment_list.push(matchingEquipment);
+                }
+            });
+
+            // get list of powers
+            let powers_list = [];
+            weirdo.powers.forEach(pwr => {
+                const matchingPower = powers.find(list_pwr => list_pwr.name === pwr);
+                if (matchingPower) {
+                    powers_list.push(matchingPower);
+                }
+            });
+
+            // combine powers and equipment to one list
+            let items_list = equipment_list.concat(powers_list);
+
+            if (items_list.length == 0) {
+                equipment_area.innerHTML = '&ensp;-'
+            } else {
+                for (const item of items_list) {
+                    let template_item = new_card.querySelector('#item_template');
+                    let item_card = template_item.cloneNode(true); // clear out events
+                    let hide_long = ['Effect', 'Attack', 'Action']
+                    item_card.removeAttribute("hidden");
+                    item_card.removeAttribute("id");
+                    item_card.querySelector('.name').innerHTML = `${item.name}`;
+                    if (document.getElementById('eq_desc').checked && hide_long.includes(item.type) && item.notes.length > 70) {
+                        item_card.querySelector('.notes').innerHTML = `(${item.type}) Information on how to use in book.` //item.notes.slice(0, 70) + "...";
+                    } else {
+                        item_card.querySelector('.notes').innerHTML = `(${item.type})  ${item.notes}`;
+                    }
+                    equipment_area.appendChild(item_card)
+                }
+            }
+
+            card_container.appendChild(new_card);
+        }
+    });
+}
+
+
+
